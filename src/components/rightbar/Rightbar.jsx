44dcdcb1b1a0f9f03ddb67f0babe092154160896
TarushGupta23 <tarush23.gupta@gmail.com>
2024-01-30 22:07:45 +0530

    complete rightbar

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -4,7 +4,25 @@ import "./rightbar.css"
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-      
+      <div className="rightbarwrapper">
+        <div className="birthdaycontainer">
+          <img src="assets/gift.png" alt="" className="birthdayimg" />
+          <span className="birthdaytext">
+            <b>Sumit Sati</b> and <b>3 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="" className="rightbarad" />
+        <h4 className="rightbartitle">Online Friends</h4>
+        <ul className="rightbarfriendslist">
+          <li className="frienditem">
+            <div className="rightbarporfileimgcontainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarprofileimg" />
+              <span className="rightbaronline"></span>
+            </div>
+            <span className='rightbarusername'>Joen Carter</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
