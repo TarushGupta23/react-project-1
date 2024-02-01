8c04209aa0da7291592d9eea7d1130a22b575650
TarushGupta23 <tarush23.gupta@gmail.com>
2024-02-01 21:26:56 +0530

    add posts dynamically from js

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import "./feed.css"
 import Share from '../share/Share'
 import Post from '../post/Post'
+import {Posts} from "../../dummyData.js"
 
 export default function Feed() {
   return (
     <div className='feed'>
       <div className="feedwrapper">
         <Share />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {Posts.map(p=> {return <Post key={p.id} post={p} />} )}
       </div>
     </div>
   )
